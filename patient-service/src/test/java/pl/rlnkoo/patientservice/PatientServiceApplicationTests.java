c7686b780d9635f54914a185eaba2b3e3b26134f
Artur Rolak <rolakartur96@gmail.com>
2025-04-22 16:41:43 +0200

    Project started

---
+++ patient-service/src/test/java/pl/rlnkoo/patientservice/PatientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.rlnkoo.patientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
