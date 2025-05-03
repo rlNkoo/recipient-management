843e7b25816a30a69cee90a340059a5f46ad5f06
Artur Rolak <rolakartur96@gmail.com>
2025-05-03 14:46:30 +0200

    Jwt validation added

--- auth-service/src/main/java/pl/rlnkoo/authservice/controller/AuthController.java
+++ auth-service/src/main/java/pl/rlnkoo/authservice/controller/AuthController.java
@@ -3,9 +3,7 @@ package pl.rlnkoo.authservice.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.rlnkoo.authservice.dto.LoginRequestDTO;
 import pl.rlnkoo.authservice.dto.LoginResponseDTO;
 import pl.rlnkoo.authservice.service.AuthService;
@@ -33,5 +31,18 @@ public class AuthController {
 
         String token = tokenOptional.get();
         return ResponseEntity.ok(new LoginResponseDTO(token));
+    }
+
+    @Operation(summary = "Validate Token")
+    @GetMapping("/validate")
+    public ResponseEntity<Void> validateToken(@RequestHeader("Authorization") String authHeader) {
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        return authService.validateToken(authHeader.substring(7))
+                ? ResponseEntity.ok().build()
+                : ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 }
